ac1e585e3239560646cf34e7628491d60f0de797
James Loh <git@jloh.co>
2022-02-21 18:49:27 +1100

    more attempts

--- src/hooks.js
+++ src/hooks.js
@@ -1,9 +1,9 @@
 /** @type {import('@sveltejs/kit').GetSession} */
 export function getSession(event) {
 	console.log(event.request.headers)
-	if (event.request.headers['client-ip']) {
+	if (event.request.headers['x-nf-client-connection-ip'] !== null) {
 		return {
-				userIP: event.request.headers['client-ip']
+				userIP: event.request.headers['x-nf-client-connection-ip']
 		}
 	}
 }
