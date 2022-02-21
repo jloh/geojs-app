0c06895f48a6b5eb1c45d63abc3cc81f617f8b0e
James Loh <git@jloh.co>
2022-02-21 18:47:06 +1100

    Add debug for headers

--- src/hooks.js
+++ src/hooks.js
@@ -1,5 +1,6 @@
 /** @type {import('@sveltejs/kit').GetSession} */
 export function getSession(event) {
+	console.log(event.request.headers)
 	if (event.request.headers['client-ip']) {
 		return {
 				userIP: event.request.headers['client-ip']
