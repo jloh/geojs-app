ca472399fcb12ac309359aa8a643d20c31335919
James Loh <git@jloh.co>
2022-02-21 18:51:57 +1100

    debug

--- src/hooks.js
+++ src/hooks.js
@@ -2,6 +2,8 @@
 export function getSession(event) {
 	console.log(event.request.headers)
 	if (event.request.headers['x-nf-client-connection-ip'] !== null) {
+		console.log(event.request.headers['x-nf-client-connection-ip'])
+		console.log("in here")
 		return {
 				userIP: event.request.headers['x-nf-client-connection-ip']
 		}
