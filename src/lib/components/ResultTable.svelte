babf524690e5e083fc9adebd35d2c73853222f5d
James Loh <git@jloh.co>
2022-02-24 18:47:16 +1100

    fix title scrolling with table

--- src/lib/components/ResultTable.svelte
+++ src/lib/components/ResultTable.svelte
@@ -3,55 +3,57 @@
 	export let ptr;
 </script>
 
-<div class="overflow-auto">
+<div class="">
 	<h1 class="text-center my-2 text-bold text-lg font-medium">Results for {geo.ip}</h1>
-	<table class="border rounded w-full table-auto">
-		<tbody>
-			<tr>
-				<th>IP Address</th>
-				<td><code>{geo.ip}</code></td>
-			</tr>
-			<tr>
-				<th>Organization</th>
-				<td>{geo.organization_name || 'Organization unknown'}</td>
-			</tr>
-			<tr>
-				<th>Country</th>
-				<td>{geo.country || 'Country unknown'}</td>
-			</tr>
-			<tr>
-				<th>City</th>
-				<td>{geo.city || 'No city found'}</td>
-			</tr>
-			<tr>
-				<th>Region</th>
-				<td>{geo.region || 'No region found'}</td>
-			</tr>
-			<tr>
-				<th>PTR</th>
-				<td>{#await ptr}Loading...{:then ptr}{ptr.ptr}{/await}</td>
-			</tr>
-			<tr>
-				<th>Latitude</th>
-				<td>{geo.latitude || 'Latitude unknown'}</td>
-			</tr>
-			<tr>
-				<th>Longitude</th>
-				<td>{geo.longitude || 'Longitude unknown'}</td>
-			</tr>
-			<tr>
-				<th>ASN</th>
-				<td
-					>{#if geo.asn}<a
-							href="https://www.peeringdb.com/search?q={geo.asn}"
-							title="Peering DB search for {geo.asn}"
-							target="_blank"
-							class="text-blue-500 hover:text-blue-600 transition-all duration-100">{geo.asn}</a
-						>{:else}No ASN found{/if}</td
-				>
-			</tr>
-		</tbody>
-	</table>
+	<div class="overflow-auto">
+		<table class="border rounded w-full table-auto">
+			<tbody>
+				<tr>
+					<th>IP Address</th>
+					<td><code>{geo.ip}</code></td>
+				</tr>
+				<tr>
+					<th>Organization</th>
+					<td>{geo.organization_name || 'Organization unknown'}</td>
+				</tr>
+				<tr>
+					<th>Country</th>
+					<td>{geo.country || 'Country unknown'}</td>
+				</tr>
+				<tr>
+					<th>City</th>
+					<td>{geo.city || 'No city found'}</td>
+				</tr>
+				<tr>
+					<th>Region</th>
+					<td>{geo.region || 'No region found'}</td>
+				</tr>
+				<tr>
+					<th>PTR</th>
+					<td>{#await ptr}Loading...{:then ptr}{ptr.ptr}{/await}</td>
+				</tr>
+				<tr>
+					<th>Latitude</th>
+					<td>{geo.latitude || 'Latitude unknown'}</td>
+				</tr>
+				<tr>
+					<th>Longitude</th>
+					<td>{geo.longitude || 'Longitude unknown'}</td>
+				</tr>
+				<tr>
+					<th>ASN</th>
+					<td
+						>{#if geo.asn}<a
+								href="https://www.peeringdb.com/search?q={geo.asn}"
+								title="Peering DB search for {geo.asn}"
+								target="_blank"
+								class="text-blue-500 hover:text-blue-600 transition-all duration-100">{geo.asn}</a
+							>{:else}No ASN found{/if}</td
+					>
+				</tr>
+			</tbody>
+		</table>
+	</div>
 </div>
 
 <style>
