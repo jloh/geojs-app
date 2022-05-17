25479223649bca87f63e7804074bdcc39fac45bf
James Loh <git@jloh.co>
2022-05-17 21:48:43 +1000

    simplify nav classes

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -6,21 +6,17 @@
 	<nav id="header" class="mx-auto max-w-6xl py-2 px-4">
 		<div class="mx-auto mb-2 flex flex-wrap">
 			<div
-				class="mt-2 flex flex-1 items-baseline justify-center sm:justify-start sm:text-left md:w-auto"
+				class="mt-2 flex flex-1 items-baseline justify-center text-lg font-medium text-gray-800 transition-all sm:justify-start sm:text-left md:w-auto md:font-normal"
 			>
 				<a title="Home" href="/"
 					><img class="-mb-1 max-h-8" alt="GeoJS" src="https://www.geojs.io/img/logo.png" /></a
 				>
 				<a
-					class="ml-3 text-lg text-gray-800 transition-all hover:text-blue-500"
+					class="ml-3 hover:text-blue-500"
 					title="GeoJS documentation"
 					href="https://geojs.io/docs/">Docs</a
 				>
-				<a
-					class="ml-3 text-lg text-gray-800 transition-all hover:text-blue-500"
-					title="Home"
-					href="https://jloh.co/tag/geojs/">Blog</a
-				>
+				<a class="ml-3 hover:text-blue-500" title="Home" href="https://jloh.co/tag/geojs/">Blog</a>
 			</div>
 			<div
 				class="mt-2 flex-grow items-center justify-center sm:justify-end lg:flex lg:w-auto"
