94240146e625fc7e6f40b95449d4883c79c996bd
James Loh <git@jloh.co>
2022-05-19 08:13:46 +1000

    Add support for Cloudflare pages

--- svelte.config.js
+++ svelte.config.js
@@ -1,4 +1,4 @@
-import adapter from '@sveltejs/adapter-netlify';
+import adapter from '@sveltejs/adapter-auto';
 import preprocess from 'svelte-preprocess';
 
 /** @type {import('@sveltejs/kit').Config} */
@@ -9,7 +9,6 @@ const config = {
 
 	kit: {
 		adapter: adapter({
-			edge: false
 		}),
 		prerender: {
 			enabled: false
