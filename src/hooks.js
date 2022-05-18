90f869c33fb2e730ee3fa00453c890b6335d9ed0
James Loh <git@jloh.co>
2022-05-18 22:06:54 +1000

    more debug

--- src/hooks.js
+++ src/hooks.js
@@ -1,6 +1,7 @@
 /** @type {import('@sveltejs/kit').GetSession} */
 export function getSession(event) {
 	console.log(event)
+	console.log(event.clientAddress)
 	if (event.clientAddress !== null) {
 		console.log(event.clientAddress)
 	} else {
