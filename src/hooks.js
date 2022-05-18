b1fead473846e70368498251ae08d19c2733635f
James Loh <git@jloh.co>
2022-05-18 22:24:27 +1000

    Fix getting initial IP
    turns out netlify edge funcs are just broken and don't contain the requesting IP address at this stage

--- src/hooks.js
+++ src/hooks.js
@@ -1,16 +1,8 @@
 /** @type {import('@sveltejs/kit').GetSession} */
 export function getSession(event) {
-	console.log(event)
-	console.log(event.clientAddress)
-	console.log(typeof event.clientAddress)
 	if (event.clientAddress !== null) {
-		console.log(event.clientAddress)
-	} else {
-		console.log("No clientAddress")
-	}
-	if (event.request.headers.get('x-nf-client-connection-ip') !== null) {
 		return {
-			userIP: event.request.headers.get('x-nf-client-connection-ip')
+			userIP: event.clientAddress
 		}
 	} else {
 		return {
