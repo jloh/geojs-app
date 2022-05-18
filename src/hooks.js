a5360847fd6b2630062e723e9329faee1e9d63de
James Loh <git@jloh.co>
2022-05-18 21:44:37 +1000

    more debug

--- src/hooks.js
+++ src/hooks.js
@@ -1,5 +1,6 @@
 /** @type {import('@sveltejs/kit').GetSession} */
 export function getSession(event) {
+	console.log(event)
 	if (event.request.headers.get('x-nf-client-connection-ip') !== null) {
 		console.log(event.request.headers.get('x-nf-client-connection-ip'))
 		console.log(event)
