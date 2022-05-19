f1844e2c9ed721ffce7f64f7ae713b977ffc0a71
James Loh <git@jloh.co>
2022-05-19 21:11:26 +1000

    vercel doesn't support IPv6, CF didn't work for some reason

--- svelte.config.js
+++ svelte.config.js
@@ -1,4 +1,4 @@
-import adapter from '@sveltejs/adapter-auto';
+import adapter from '@sveltejs/adapter-netlify';
 import preprocess from 'svelte-preprocess';
 
 /** @type {import('@sveltejs/kit').Config} */
@@ -9,6 +9,7 @@ const config = {
 
 	kit: {
 		adapter: adapter({
+			edge: false
 		}),
 		prerender: {
 			enabled: false
