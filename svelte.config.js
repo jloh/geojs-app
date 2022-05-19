2e11fa9d4df443afa905cd6554e2cbd04a21f56d
James Loh <git@jloh.co>
2022-05-19 22:30:04 +1000

    Update CSP and port into Svelte

--- svelte.config.js
+++ svelte.config.js
@@ -8,6 +8,19 @@ const config = {
 	preprocess: preprocess(),
 
 	kit: {
+		csp: {
+			directives: {
+				'default-src': ['none'],
+				'script-src': ['self', 'get.geojs.io', 'cdn.simpleanalytics.io', 'sa.geojs.io'],
+				'style-src': ['self', 'unsafe-inline'],
+				'img-src': ['self', 'www.geojs.io'],
+				'connect-src': ['self', 'get.geojs.io', 'sa.geojs.io', 'api.simpleanalytics.io'],
+				'font-src': ['none'],
+				'form-action': ['self'],
+				'upgrade-insecure-requests': true,
+				'report-uri': ['https://jloh.report-uri.com/r/d/csp/enforce']
+			}
+		},
 		adapter: adapter({
 			edge: false
 		}),
