30f3ea2b974434d20803c86b66f9e69a1b13ebaf
James Loh <git@jloh.co>
2022-05-20 18:02:18 +1000

    do dual deploy between netlify and cloudflare

--- svelte.config.js
+++ svelte.config.js
@@ -1,4 +1,4 @@
-import adapter from '@sveltejs/adapter-netlify';
+import adapter from '@sveltejs/adapter-auto';
 import preprocess from 'svelte-preprocess';
 
 /** @type {import('@sveltejs/kit').Config} */
@@ -22,7 +22,7 @@ const config = {
 			}
 		},
 		adapter: adapter({
-			edge: false
+			// edge: false // Used to enable Netlify edge funcs
 		}),
 		prerender: {
 			enabled: false
