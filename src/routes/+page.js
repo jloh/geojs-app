09a65c59bfa9098b12ec17e0cfdd2225d5856b5a
James Loh <git@jloh.co>
2022-08-26 14:11:42 +1000

    Fix homepage rendering now $session is no longer
    See https://github.com/sveltejs/kit/discussions/5883

--- src/routes/+page.js
+++ src/routes/+page.js
@@ -1,15 +1,8 @@
-import { browser } from '$app/env';
-
 /** @type {import('./$types').PageLoad} */
-export async function load({ fetch, session }) {
+export async function load({ fetch }) {
 	var geo, ptr;
-	if (!browser) {
-		geo = `https://get.geojs.io/v1/ip/geo/${session.userIP}.json`;
-		ptr = `https://get.geojs.io/v1/dns/ptr/${session.userIP}.json`;
-	} else {
-		geo = 'https://get.geojs.io/v1/ip/geo.json';
-		ptr = 'https://get.geojs.io/v1/dns/ptr.json';
-	}
+	geo = 'https://get.geojs.io/v1/ip/geo.json';
+	ptr = 'https://get.geojs.io/v1/dns/ptr.json';
 	const geoResponse = await fetch(geo);
 	const ptrResponse = await fetch(ptr);
 	return {
