cb26d1178d7eb2824559bb136d175d44b772e292
James Loh <git@jloh.co>
2022-12-16 00:45:45 +1100

    more changes to fix hook

--- src/hooks.server.js
+++ src/hooks.server.js
@@ -1,12 +1,15 @@
 /** @type {import('@sveltejs/kit').HandleFetch} */
 export async function handleFetch({ request, fetch, event }) {
 	if (request.url == 'https://get.geojs.io/v1/ip/geo.json') {
-		// clone the original request, but change the URL
-		request = new Request(`https://get.geojs.io/v1/ip/geo/${event.getClientAddress()}.json`, request)
-	} else if (request.url === 'https://get.geojs.io/v1/dns/ptr.json') {
-		// clone the original request, but change the URL
-		request = new Request(`https://get.geojs.io/v1/dns/ptr/${event.getClientAddress()}.json`, request)
+		request = new Request(
+			`https://get.geojs.io/v1/ip/geo/${event.getClientAddress()}.json`,
+			request
+		)
+	} else if (request.url == 'https://get.geojs.io/v1/dns/ptr.json') {
+		request = new Request(
+			`https://get.geojs.io/v1/dns/ptr/${event.getClientAddress()}.json`,
+			request
+		)
 	}
-
 	return fetch(request);
 }
