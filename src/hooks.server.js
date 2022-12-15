d18670075d3326c6ecb6fd684b9cd17937ec65aa
James Loh <git@jloh.co>
2022-12-16 00:36:20 +1100

    Cleanup code to try and fix 500s

--- src/hooks.server.js
+++ src/hooks.server.js
@@ -1,17 +1,11 @@
 /** @type {import('@sveltejs/kit').HandleFetch} */
 export async function handleFetch({ request, fetch, event }) {
-	if (request.url === 'https://get.geojs.io/v1/ip/geo.json') {
+	if (request.url == 'https://get.geojs.io/v1/ip/geo.json') {
 		// clone the original request, but change the URL
-		request = new Request(
-			request.url.replace('https://get.geojs.io/v1/ip/geo.json', `https://get.geojs.io/v1/ip/geo/${event.getClientAddress()}.json`),
-			request
-		);
+		request = new Request(`https://get.geojs.io/v1/ip/geo/${event.getClientAddress()}.json`, request)
 	} else if (request.url === 'https://get.geojs.io/v1/dns/ptr.json') {
 		// clone the original request, but change the URL
-		request = new Request(
-			request.url.replace('https://get.geojs.io/v1/dns/ptr.json', `https://get.geojs.io/v1/dns/ptr/${event.getClientAddress()}.json`),
-			request
-		);
+		request = new Request(`https://get.geojs.io/v1/dns/ptr/${event.getClientAddress()}.json`, request)
 	}
 
 	return fetch(request);
