cf4079935dac34e8020733f64e9b36de7dc9ac24
James Loh <git@jloh.co>
2024-03-29 14:35:17 +1100

    Update PTR handling

--- src/lib/geojs.js
+++ src/lib/geojs.js
@@ -1,13 +1,13 @@
 'use strict';
 
 /**
-* @public
-* @function getGeo
-* @description Gets Geolcoation information for an IP from the GeoJS API
-* @param {String} address - IP address.
-* @param {RequestHandler} fetchURL - Fetch URL API
-* @returns {Array} -
-*/
+ * @public
+ * @function getGeo
+ * @description Gets Geolcoation information for an IP from the GeoJS API
+ * @param {String} address - IP address.
+ * @param {RequestHandler} fetchURL - Fetch URL API
+ * @returns {Array} -
+ */
 
 export async function getGeo(fetchURL, address) {
 	let endpoint = 'https://get.geojs.io/v1/ip/geo.json';
@@ -25,24 +25,17 @@ export async function getGeo(fetchURL, address) {
 }
 
 /**
-* @public
-* @function getPTR
-* @description Gets a PTR
-* @param {String} address - IP address.
-* @returns {Promise} - Determine whether IP address is valid.
-*/
+ * @public
+ * @function getPTR
+ * @description Gets a PTR
+ * @param {String} address - IP address.
+ * @returns {Promise<Response>} - Determine whether IP address is valid.
+ */
 
 export async function getPTR(fetchURL, address) {
-	let endpoint = 'https://get.geojs.io/v1/dns/ptr.json';
+	let endpoint = 'https://get.geojs.io/v1/dns/ptr';
 	if (address != null) {
-		endpoint = `https://get.geojs.io/v1/dns/ptr/${address}.json`;
+		endpoint = `https://get.geojs.io/v1/dns/ptr/${address}`;
 	}
-	const res = await fetchURL(endpoint);
-	const resp = await res.json();
-
-	if (res.ok) {
-		return resp;
-	} else {
-		throw new Error(resp);
-	}
+	return fetchURL(endpoint);
 }
